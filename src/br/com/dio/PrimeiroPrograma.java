cdac8706f5e36293c08318d055ac295a4272b9d6
Larissa <larissauchoap11@gmail.com>
2022-05-12 17:07:43 -0300

    Primeiro commit java

---
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,14 @@
+package br.com.dio;
+
+public class PrimeiroPrograma {
+
+	public static void main(String[] args) {
+		int a=2;
+	    int b=3 ;
+	    int total= a+b;
+		
+		System.out.println("Hello world , o resultado e " + (total));
+		
+	}
+
+}
